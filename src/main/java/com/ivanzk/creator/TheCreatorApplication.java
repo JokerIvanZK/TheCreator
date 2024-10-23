1ecd1457dab642e3b08d37ef36eecc59ed12deb8
zk <1184253234@qq.com>
2024-10-23 14:33:52 +0800

    init

---
+++ src/main/java/com/ivanzk/creator/TheCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.ivanzk.creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCreatorApplication.class, args);
+    }
+
+}
